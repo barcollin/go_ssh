ccc30b6c06c3df1495cb7cf115e77f8b230ea8b8
barcollin jay <jubilates@hotmail.com>
2023-09-14 17:37:06 +0200

    modified StartServer method of server

--- server.go
+++ server.go
@@ -1,5 +1,24 @@
 package ssh
 
+import (
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+)
+
 func StartServer(privateKey []byte, authorizedKeys []byte) error {
+	authorizedKeysMap := map[string]bool{}
+
+	for len(authorizedKeys) > 0 {
+		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeys)
+
+		if err != nil {
+			return fmt.Errorf("Parse Authorized keys error: %s", err)
+		}
+
+		authorizedKeysMap[string(pubKey.Marshal())] = true
+		authorizedKeys = rest
+	}
+
 	return nil
 }
