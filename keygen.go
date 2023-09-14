2e94ac15c2070ab41cd9b5fa1268baa3919d17e9
barcollin jay <jubilates@hotmail.com>
2023-09-14 10:44:39 +0200

    modified GenerateKeys function

--- keygen.go
+++ keygen.go
@@ -1,5 +1,23 @@
 package ssh
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+)
+
 func GenerateKeys() ([]byte, []byte, error) {
-	return nil, nil, nil
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, fmt.Errorf("GenerateKey error: %s", err)
+	}
+
+	privateKeyPem := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	return privateKeyPem.Bytes, nil, nil
 }
