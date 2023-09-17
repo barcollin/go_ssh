3bdb8cb14b1510d093b968cdf3511b740cb24d7f
barcollin jay <jubilates@hotmail.com>
2023-09-17 06:57:54 +0200

    modified server file

--- server.go
+++ server.go
@@ -10,10 +10,8 @@ import (
 
 func StartServer(privateKey []byte, authorizedKeys []byte) error {
 	authorizedKeysMap := map[string]bool{}
-
 	for len(authorizedKeys) > 0 {
 		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeys)
-
 		if err != nil {
 			return fmt.Errorf("Parse Authorized keys error: %s", err)
 		}
@@ -26,38 +24,40 @@ func StartServer(privateKey []byte, authorizedKeys []byte) error {
 		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
 			if authorizedKeysMap[string(pubKey.Marshal())] {
 				return &ssh.Permissions{
+					// Record the public key used for authentication.
 					Extensions: map[string]string{
 						"pubkey-fp": ssh.FingerprintSHA256(pubKey),
 					},
 				}, nil
 			}
-			return nil, fmt.Errorf("Unknown public key for %q", c.User())
+			return nil, fmt.Errorf("unknown public key for %q", c.User())
 		},
 	}
 
 	private, err := ssh.ParsePrivateKey(privateKey)
 	if err != nil {
-		return fmt.Errorf("Failed to parse private key %s", err)
+		return fmt.Errorf("ParsePrivateKey error: %s", err)
 	}
 
 	config.AddHostKey(private)
 
+	// accepted.
 	listener, err := net.Listen("tcp", "0.0.0.0:2022")
 	if err != nil {
-		return fmt.Errorf("Failed to listen for connection %s", err)
+		return fmt.Errorf("Listen error: %s", err)
 	}
 
 	nConn, err := listener.Accept()
 	if err != nil {
-		return fmt.Errorf("Failed to accept incoming connection %s", err)
+		fmt.Printf("Listener accept error: %s\n", err)
 	}
 
 	conn, _, _, err := ssh.NewServerConn(nConn, config)
 	if err != nil {
-		return fmt.Errorf("Failed to accept incoming connection %s", err)
+		fmt.Printf("NewServerConn error: %s\n", err)
 	}
-
 	log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+
 	return nil
 
 }
